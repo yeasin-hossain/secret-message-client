87ac6e5a070c30587efccf15abbfd602247e5600
Yeasin Hossain <50882838+yeasin-hossain@users.noreply.github.com>
2021-08-25 17:56:08 +0600

    add go for test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ToastContainer } from 'react-toastify';
 import './customStyle/customStyle.css';
 import RouterWrapper from './routes/RouterWraper';
+import { Link } from 'react-router-dom';
 
 function App() {
     return (
         <>
+        <Link to='sheradeals://Shop/Daraz' >Go</Link>  
             <ToastContainer position="top-center" />
             <RouterWrapper />
         </>
